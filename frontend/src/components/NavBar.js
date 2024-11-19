a800c5467e70b0ce4a0681b7ed0715e260bd2c53
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-20 02:26:52 +0300

    feat: update Navbar with links to new pages - Add links for Crop Diagnostics, Educational Resources, and Payment pages - Ensure consistent styling and structure

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <nav className="bg-green-600 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">Becof</Link>
-        <ul className="flex space-x-4">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/marketplace">Marketplace</Link></li>
-          <li><Link to="/forum">Forum</Link></li>
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="bg-green-600 text-white p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link to="/" className="text-xl font-bold">Becof</Link>
+                <ul className="flex space-x-4">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/marketplace">Marketplace</Link></li>
+                    <li><Link to="/forum">Forum</Link></li>
+                    <li><Link to="/crop-diagnostics">Crop Diagnostics</Link></li>
+                    <li><Link to="/educational-resources">Educational Resources</Link></li>
+                    <li><Link to="/payment">Payment</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
